0e4746c80e6457fa3f9a1b87573f66c3fb6b5bd9
sbmose <superbright43@gmail.com>
2021-03-27 01:37:42 +0100

    installed redux, react-redux, @reduxjs/toolkit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	useHistory,
+} from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ProgressBar from './components/ProgressBar';
 
-import SuccessPage from './views/SuccessPage';
 import LandingPage from './views/LandingPage';
+import FormsPage from './views/FormsPage';
+import SuccessPage from './views/SuccessPage';
 
 const App = () => {
 	return (
 		<>
-			<Navbar />
-			<LandingPage />
-			<Footer />
+			<Router>
+				<Navbar />
+
+				<Switch>
+					<Route exact path='/'>
+						<LandingPage />
+					</Route>
+
+					<Route path='/success'>
+						<SuccessPage></SuccessPage>
+					</Route>
+
+					<Route path='/forms'>
+						<FormsPage />
+					</Route>
+				</Switch>
+
+				<Footer />
+			</Router>
 		</>
 	);
 };
